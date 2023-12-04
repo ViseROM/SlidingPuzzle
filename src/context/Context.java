58478f7fd3168926ebe14e009c7c4b130d640eac
Vachia Thoj <47841533+ViseROM@users.noreply.github.com>
2023-12-04 17:14:50 -0600

    Add files via upload

--- src/context/Context.java
+++ src/context/Context.java
@@ -1,1 +1,34 @@
+package context;
 
+import backend.Game;
+
+/**
+ * Context connects the front end to back end 
+ * 
+ * @author Vachia Thoj
+ *
+ */
+public class Context
+{
+	//Game object
+	private Game game;
+	
+	/**
+	 * Constructor
+	 */
+	public Context()
+	{
+		this.game = new Game();
+	}
+	
+	//Getter methods
+	public int getNumRows() {return this.game.getNumRows();}
+	public int getNumCols() {return this.game.getNumCols();}
+	public int[][] getSolvedPuzzle() {return this.game.getSolvedPuzzle();}
+	public int[][] getShuffledPuzzle() {return this.game.getShuffledPuzzle();}
+	public boolean isGameOver() {return this.game.isGameOver();}
+	
+	public void shuffle() {this.game.shuffle();}
+	public void swap(int row1, int col1, int row2, int col2) {this.game.swap(row1, col1, row2, col2);}
+	public void checkForWin() {this.game.checkForWin();}
+}
