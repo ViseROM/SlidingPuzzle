9374ae7874b280f07a1691a0c5390a1e6db4c93b
Vachia Thoj <47841533+ViseROM@users.noreply.github.com>
2023-12-04 17:15:51 -0600

    Add files via upload

---
+++ src/main/GamePanel.java
@@ -0,0 +1,170 @@
+package main;
+
+import java.awt.Dimension;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.Graphics2D;
+import java.awt.Graphics;
+import java.awt.image.*;
+import java.awt.RenderingHints;
+import javax.swing.JPanel;
+
+import manager.MouseManager;
+import manager.StateManager;
+
+/**
+ * GamePanel class is the panel (screen) that will be drawn on
+ * Contains the game loop
+ * @author Vachia Thoj
+ *
+ */
+public class GamePanel extends JPanel implements Runnable
+{
+	private static final long serialVersionUID = 1L;
+	
+	//Width and height of GamePanel
+	public static final int WIDTH = 720;
+	public static final int HEIGHT = 720;
+	
+	//Scale to adjust the width and height of the GamePanel
+	public static final int SCALE = 1;
+	
+	//Thread to run the game
+	private Thread thread;
+	private boolean running;
+	
+	//To render graphics
+	private BufferedImage image;
+	private Graphics2D g;
+	
+	//Game framerate
+	private static final int FPS = 60;
+	private static final int TARGET_TIME = 1000 / FPS;
+	
+	//To manage mouse events
+	private MouseManager mouseManager;
+	
+	//To manager different states
+	private StateManager stateManager;
+	
+	/**
+	 * Constructor
+	 */
+	public GamePanel()
+	{
+		super();
+		this.setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		this.setFocusable(true);
+		this.requestFocus();
+		
+		mouseManager = MouseManager.instance();
+		stateManager = StateManager.instance();
+	}
+	
+	public void addNotify()
+	{
+		super.addNotify();
+		
+		if(thread == null)
+		{
+			//Create thread and start thread
+			thread = new Thread(this);
+			thread.start();
+			
+			//Add a MouseListener
+			addMouseListener(new MouseAdapter()
+			{
+				public void mousePressed(MouseEvent e)
+				{
+					mouseManager.setPressedPoint(e.getX(), e.getY());
+					mouseManager.setMousePressed(true);
+					mouseManager.setMouseReleased(false);
+				}
+				
+				public void mouseReleased(MouseEvent e)
+				{
+					mouseManager.setReleasedPoint(e.getX(), e.getY());
+					mouseManager.setMouseReleased(true);
+					mouseManager.setMousePressed(false);
+				}
+			});
+			
+			//Add a MouseMotionListener
+			addMouseMotionListener(new MouseAdapter() 
+			{
+				public void mouseMoved(MouseEvent e)
+				{
+					mouseManager.setCurrentPoint(e.getX(), e.getY());
+				}
+			});
+		}
+	}
+	
+	public void run()
+	{
+		running = true;
+		
+		image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+		g = (Graphics2D) image.getGraphics();
+		
+		//Add anti-aliasing
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		
+		long start;
+		long elapsed;
+		long wait;
+		
+		//Events to do while thread is running
+		while(running)
+		{
+			start = System.nanoTime();
+			
+			update();
+			draw();
+			drawToScreen();
+			
+			//******* (START) Frame counting *******
+			elapsed = (System.nanoTime() - start) / 1000000;
+			wait = TARGET_TIME - elapsed;
+			
+			if(wait < 0)
+			{
+				wait = TARGET_TIME;
+			}
+			
+			try{
+				Thread.sleep(wait);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+			//******* (END) Frame counting *******
+		}
+	}
+	
+	/**
+	 * Method to update screen
+	 */
+	private void update()
+	{
+		stateManager.update();
+	}
+	
+	/**
+	 * Method to draw on screen
+	 */
+	private void draw()
+	{
+		stateManager.draw(g);
+	}
+	
+	/**
+	 * Method for double buffering
+	 */
+	private void drawToScreen()
+	{
+		Graphics g2 = this.getGraphics();
+		g2.drawImage(image, 0, 0, WIDTH * SCALE, HEIGHT * SCALE, null);
+		g2.dispose();
+	}
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,1 +1,35 @@
+package main;
 
+import javax.swing.JFrame;
+
+/**
+ * Main is the class that you run/execute in order to start the program
+ *  
+ * @author Vachia Thoj
+ */
+public class Main 
+{
+	public static void main(String[] args)
+	{
+		//Create Window
+		JFrame window = new JFrame("Sliding Puzzle");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//Create GamePanel
+		GamePanel gamePanel = new GamePanel();
+		
+		//Store gamePanel to content pane
+		window.setContentPane(gamePanel);
+		
+		window.pack();
+		
+		//Do not window to be resizeable
+		window.setResizable(false);
+		
+		//Center window to computer screen
+		window.setLocationRelativeTo(null);
+		
+		//Make window visible
+		window.setVisible(true);
+	}
+}
